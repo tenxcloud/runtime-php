e9285cb671d4fd6b63e0c5fa16f17fa5c0ad543d
huangqg <csuhp007@gmail.com>
2016-02-21 18:59:25 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# runtime-php
+hello-world-lamp
+================
+
+Hello world in PHP to test LAMP deployments
---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Hello world!</title>
+	<style>
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Helvetica Neue",Helvetica,"Microsoft YaHei",Arial,"宋体","Hiragino Sans GB",STHeiti,"WenQuanYi Micro Hei",SimSun,sans-serif;
+	}
+
+	#logo {
+		margin-bottom: 40px;
+		width: 300px;
+	}
+	</style>
+</head>
+<body>
+	<img id="logo" src="logo.png" />
+	<h1><?php echo "Hello world!"; ?></h1>
+	<h2><?php echo "PHP 运行时环境，包含 Ubuntu + Apache + PHP5，不自带 Mysql 数据库"; ?></h2>
+</body>
+</html>
